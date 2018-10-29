a7b6dc169c116d407bebd1f535754472fd22f6c6
mariusgg <marius.g.guei@gmail.com>
2018-10-29 00:56:13 +0000

    spec helper refactor ...

--- spec/features/ChitterApp_spec.rb
+++ spec/features/ChitterApp_spec.rb
@@ -2,34 +2,15 @@ require 'chitter'
 
 describe Chitter do
 
-  subject(:chitter) { described_class.new}
-
-  describe '#init' do
-    it "returns username" do
-      expect(chitter.username).to eq "Mariusgg14"
-    end
-
-    it "returns a text input" do
-      expect(chitter.text).to eq "I'm hungry"
-    end
-
-    it "ireturns the date" do
-      expect(chitter.date).to eq "28/10/2018"
-    end
-
-    it "returns the time" do
-      expect(chitter.time).to eq "22:30"
-    end
-    
-  end
+  subject(:chitter) { described_class.new( "hello people", "Mariusgg9", "2018-10-29", "00:16:00" )}
 
   describe '.add' do
     it 'returns added peep and see the time of peep' do
-      described_class.add(nil,'I want pizza tonight ')
-      described_class.add('No! I want pasta')
-      described_class.add('Maybe caribbean food :)')
+      described_class.new.add(nil,'I want pizza tonight ')
+      described_class.new.add('No! I want pasta')
+      described_class.new.add('Maybe caribbean food :)')
 
-      peeps_test = described_class.all.first
+      peeps_test = Chitter.all.first
       expect(peeps_test).to be_a Peep
       expect(peeps_test).to respond_to(:date)
       expect(peeps_test).to respond_to(:time)
--- spec/features/chitter_features_spec.rb
+++ spec/features/chitter_features_spec.rb
@@ -3,20 +3,19 @@ require 'Chitter'
 
 describe Chitter do
 
-  # describe '.all' do
-  #   it 'returns all peeps' do
-  #     chitters = Chitter.all
-  #     expect(chitters).to include("Chilling at home")
-  #     expect(chitters).to include("On my way to the pool")
-  #     expect(chitters).to include("I want pizza!")
-  #
-  #   end
-  # end
+  feature 'Add peep' do
+    scenario 'user sends a peep' do
+    visit('/')
+    fill_in 'peep', with: "I like pizza"
+    click_button 'Peep it!'
+    expect(page).to have_content "I like pizza"
+    end
+  end
 
   feature 'Posting Peeps' do
-  scenario 'I want to post a message (peep) to chitter' do
+  scenario 'I want to post a peep to chitter' do
     visit '/'
-    expect(page).to have_content 'Welcome to Chitter'
+    expect(page).to have_content 'Welcome to Chitter ; )'
 
     click_link 'Sign In'
     fill_in :username, with: 'Mariusgg14'
@@ -25,14 +24,14 @@ describe Chitter do
     expect(page).to have_link 'Mariusgg14'
     fill_in :peep, with: 'I want pizza'
 
-    click_button 'PeepIt!'
+    click_button 'Peep It!'
     expect(page).to have_content "[\"Mariusgg14\"] I want pizza"
 
     end
   end
 
   feature 'Sign out' do
-    scenario 'user signs out' do
+    scenario 'user can sign out of their account' do
       visit '/'
       click_link 'Sign In'
       fill_in :username, with: 'Mariusgg14'
